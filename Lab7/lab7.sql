ec6957a96f6c74e51cac440265a75af89b9e87db
Alexandra <75161692+Sora-ar@users.noreply.github.com>
2023-11-22 15:24:20 +0200

    Laba 7

--- Lab7/lab7.sql
+++ Lab7/lab7.sql
@@ -4,39 +4,39 @@ SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 CREATE TABLE `categories` (
-  `categoryID` int(11) NOT NULL AUTO_INCREMENT,
-  `categoryName` varchar(255) NOT NULL,
-  PRIMARY KEY (`categoryID`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `comments` (
-  `commentID` int(11) NOT NULL AUTO_INCREMENT,
-  `newsID` int(11) DEFAULT NULL,
-  `commenterName` varchar(255) NOT NULL,
-  `commentText` text NOT NULL,
-  `commentDate` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`commentID`),
-  KEY `newsID` (`newsID`),
-  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`newsID`) REFERENCES `news` (`newsID`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `news_id` int(11) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `newsID` (`news_id`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`news_id`) REFERENCES `news` (`news_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `news` (
-  `newsID` int(11) NOT NULL AUTO_INCREMENT,
-  `categoryID` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) DEFAULT NULL,
   `title` varchar(255) NOT NULL,
   `content` text NOT NULL,
-  `publicationDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `publication_date` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`newsID`),
-  KEY `categoryID` (`categoryID`),
-  CONSTRAINT `news_ibfk_1` FOREIGN KEY (`categoryID`) REFERENCES `categories` (`categoryID`)
+  KEY `categoryID` (`category_id`),
+  CONSTRAINT `news_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `ratings` (
-  `ratingID` int(11) NOT NULL AUTO_INCREMENT,
-  `newsID` int(11) DEFAULT NULL,
-  `IPaddress` varchar(45) NOT NULL,
-  `ratingValue` int(11) DEFAULT NULL CHECK (`ratingValue` between 1 and 5),
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `news_id` int(11) DEFAULT NULL,
+  `ip_address` varchar(15) NOT NULL,
+  `rating_value` int(11) DEFAULT NULL CHECK (`rating_value` between 1 and 5),
   PRIMARY KEY (`ratingID`),
-  UNIQUE KEY `newsID` (`newsID`,`IPaddress`),
-  CONSTRAINT `ratings_ibfk_1` FOREIGN KEY (`newsID`) REFERENCES `news` (`newsID`)
+  UNIQUE KEY `news_id` (`news_id`,`ip_address`),
+  CONSTRAINT `ratings_ibfk_1` FOREIGN KEY (`news_id`) REFERENCES `news` (`news_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
