ba758e5930ff0874be6b0494f9ff28cc40717d34
Alexandra <75161692+Sora-ar@users.noreply.github.com>
2023-11-22 15:42:36 +0200

    Laba 6

--- Lab6/lab6.sql
+++ Lab6/lab6.sql
@@ -12,7 +12,7 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `cityNames`;
+DROP TABLE IF EXISTS `city_names`;
 CREATE TABLE `cityNames` (
   `cityID` int(11) NOT NULL,
   `cityName` varchar(255) NOT NULL
@@ -21,25 +21,29 @@ CREATE TABLE `cityNames` (
 
 DROP TABLE IF EXISTS `connections`;
 CREATE TABLE `connections` (
-  `connectionID` int(11) NOT NULL,
-  `stationID1` int(11) NOT NULL,
-  `stationID2` int(11) NOT NULL,
-  `connectionType` varchar(255) NOT NULL
+  `id` int(11) NOT NULL,
+  `station_id1` int(11) NOT NULL,
+  `station_id2` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 DROP TABLE IF EXISTS `lines`;
 CREATE TABLE `lines` (
-  `lineID` int(11) NOT NULL,
-  `lineName` varchar(255) NOT NULL,
-  `cityID` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `city_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 DROP TABLE IF EXISTS `stations`;
 CREATE TABLE `stations` (
-  `stationID` int(11) NOT NULL,
-  `stationName` varchar(255) NOT NULL,
-  `lineID` int(11) NOT NULL,
-  `cityID` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `line_id` int(11) NOT NULL,
+  `city_id` int(11) NOT NULL
+  `sequence` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`line_id`) REFERENCES `lines` (`id`),
+  FOREIGN KEY (`city_id`) REFERENCES `city_names` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
