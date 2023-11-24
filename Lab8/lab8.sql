17bf2d0d93658644bfbff0b48cecb274d8b522e6
Alexandra <75161692+Sora-ar@users.noreply.github.com>
2023-11-25 00:42:17 +0200

    Laba 8

--- Lab8/lab8.sql
+++ Lab8/lab8.sql
@@ -4,101 +4,66 @@ SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 CREATE TABLE `files` (
-  `fileID` int(11) NOT NULL AUTO_INCREMENT,
-  `projectID` int(11) DEFAULT NULL,
-  `taskID` int(11) DEFAULT NULL,
-  `filePath` varchar(255) NOT NULL,
-  PRIMARY KEY (`fileID`),
-  KEY `projectID` (`projectID`),
-  KEY `taskID` (`taskID`),
-  CONSTRAINT `files_ibfk_1` FOREIGN KEY (`projectID`) REFERENCES `projects` (`projectID`),
-  CONSTRAINT `files_ibfk_2` FOREIGN KEY (`taskID`) REFERENCES `tasks` (`taskID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE `projectFiles` (
-  `fileID` int(11) NOT NULL AUTO_INCREMENT,
-  `projectID` int(11) DEFAULT NULL,
-  `filePath` varchar(255) NOT NULL,
-  PRIMARY KEY (`fileID`),
-  KEY `projectID` (`projectID`),
-  CONSTRAINT `projectFiles_ibfk_1` FOREIGN KEY (`projectID`) REFERENCES `projects` (`projectID`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL, 
+  `project_id` int(11) DEFAULT NULL,
+  `task_id` int(11) DEFAULT NULL,
+  `file_path` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `project_id` (`project_id`),
+  KEY `task_id` (`task_id`),
+  CONSTRAINT `files_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`),
+  CONSTRAINT `files_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE `projects` (
-  `projectID` int(11) NOT NULL AUTO_INCREMENT,
-  `projectName` varchar(255) NOT NULL,
-  PRIMARY KEY (`projectID`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-CREATE TABLE `taskAssignees` (
-  `assigneeID` int(11) NOT NULL AUTO_INCREMENT,
-  `taskID` int(11) DEFAULT NULL,
-  `userID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`assigneeID`),
-  KEY `taskID` (`taskID`),
-  KEY `userID` (`userID`),
-  CONSTRAINT `taskAssignees_ibfk_1` FOREIGN KEY (`taskID`) REFERENCES `tasks` (`taskID`),
-  CONSTRAINT `taskAssignees_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE `taskComments` (
-  `commentID` int(11) NOT NULL AUTO_INCREMENT,
-  `taskID` int(11) DEFAULT NULL,
-  `userID` int(11) DEFAULT NULL,
-  `commentText` text NOT NULL,
-  `commentDate` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`commentID`),
-  KEY `taskID` (`taskID`),
-  KEY `userID` (`userID`),
-  CONSTRAINT `taskComments_ibfk_1` FOREIGN KEY (`taskID`) REFERENCES `tasks` (`taskID`),
-  CONSTRAINT `taskComments_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE `taskFiles` (
-  `fileID` int(11) NOT NULL AUTO_INCREMENT,
-  `taskID` int(11) DEFAULT NULL,
-  `filePath` varchar(255) NOT NULL,
-  PRIMARY KEY (`fileID`),
-  KEY `taskID` (`taskID`),
-  CONSTRAINT `taskFiles_ibfk_1` FOREIGN KEY (`taskID`) REFERENCES `tasks` (`taskID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE `taskHistory` (
-  `historyID` int(11) NOT NULL AUTO_INCREMENT,
-  `taskID` int(11) DEFAULT NULL,
-  `userID` int(11) DEFAULT NULL,
-  `actionType` varchar(255) NOT NULL,
-  `actionDescription` text DEFAULT NULL,
-  `actionDate` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`historyID`),
-  KEY `taskID` (`taskID`),
-  KEY `userID` (`userID`),
-  CONSTRAINT `taskHistory_ibfk_1` FOREIGN KEY (`taskID`) REFERENCES `tasks` (`taskID`),
-  CONSTRAINT `taskHistory_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
+CREATE TABLE `task_assignees` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `task_id` (`task_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `taskAssignees_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`),
+  CONSTRAINT `taskAssignees_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE `tasks` (
-  `taskID` int(11) NOT NULL AUTO_INCREMENT,
-  `projectID` int(11) DEFAULT NULL,
-  `authorID` int(11) DEFAULT NULL,
-  `taskName` varchar(255) NOT NULL,
-  PRIMARY KEY (`taskID`),
-  KEY `projectID` (`projectID`),
-  KEY `authorID` (`authorID`),
-  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`projectID`) REFERENCES `projects` (`projectID`),
-  CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`authorID`) REFERENCES `users` (`userID`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) DEFAULT NULL,
+  `author_id` int(11) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`task_id`),
+  KEY `project_id` (`project_id`),
+  KEY `author_id` (`author_id`),
+  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`),
+  CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+CREATE TABLE `project_members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) DEFAULT NULL,
+  `author_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `project_id` (`project_id`),
+  KEY `author_id` (`author_id`),
+  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`),
+  CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE `users` (
-  `userID` int(11) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(255) NOT NULL,
-  PRIMARY KEY (`userID`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
